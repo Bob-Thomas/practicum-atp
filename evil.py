38df352c04972555f1fca7c5a8014933ce289833
Bob <smartcat007@hotmail.com>
2017-09-13 12:51:50 +0200

    evil is evil

--- evil.py
+++ evil.py
@@ -1,39 +1,57 @@
 import re
+
+from notebook.notebookapp import raw_input
+
+
 class SimpleSecureEvaluator(object):
     d = {} # empty dictionary
-    def __init__(self):
+    def __init__(self, d={}):
         # TODO
-        pass
+        self.d = d
 
     def eval(self, expressie):
+        eList = re.sub('(\(|\)|[+\-*\^\/=])', ' \\1 ', expressie).split()
         if expressie == 'mydir':
-            self.my_dir()
+            [print("%s : %s" % (k, v)) for (k, v) in self.d.items()]
+            return
+        if len(eList) == 1 and eList[0] in self.d:
+            print('%s = %s' % (eList[0], self.d[eList[0]]))
+            return
+        elif len(eList) == 1 and eList[0] not in self.d:
+            print("invalid expression")
+            print("valid expressions are")
+            [print("%s : %s" % (k, v)) for (k, v) in self.d.items()]
+            print("variable = math expression")
             return
 
-        l = expressie.strip().split(' ')
-        variable = l[0]
-        variable = self.assign(variable, l[2::])
+        if eList[1] == '=':
+            self.assign(eList[0], eList[2::])
 
-        return expressie
-
-    def my_dir(self):
-        [print('%s : %s' % (k, v)) for k,v in self.d.items()]
-
-    def assign(self, var, l):
-        for item in l:
-            if item in self.d.keys():
-                l[l.index(item)] = str(self.d[item])
-            if not re.match('^(\w+|\d|\+|\*|\-|\^)$', item):
-                print("not allowed: ", ' '.join(l))
+    def assign(self, var, expr):
+        for char in list(expr):
+            if not re.match('(\w|\(|\)|[+\-*\^\/])', char):
                 return
-        self.d[var] = eval(' '.join(l))
+        for key in self.d.keys():
+            if key in expr:
+                expr = ' '.join(expr).replace(key, str(self.d[key])).split(' ')
+        for word in expr:
+            if re.match('[a-zA-Z]', word):
+                if word not in self.d.keys():
+                    print('%s not defined' % word)
+                    return
+        result = eval(''.join(expr))
+        self.d[var] = result
+
+
 
 s = SimpleSecureEvaluator()
-s.eval('v = 1')
-s.eval('v = print(\'bad stuff\')')
-s.eval('v = 1 + 1 + 1 + 1')
-s.eval('davde = v + 20')
-s.eval('geen_dave = davde')
-s.eval('i = 1+4[eval\u0028\"print\u0028\\\"hi\\\"\u0029\u0029] +  2')
-s.eval('i = 1+2')
+s.eval('a=1')
+s.eval('b = a')
+s.eval('c = a + b')
+s.eval('nee = nope + 3')
+s.eval('nee = 2')
 s.eval('mydir')
+
+while True:
+    d = input()
+    s.eval(d)
